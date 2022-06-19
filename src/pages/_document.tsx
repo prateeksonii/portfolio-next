0297602429ab9cf5d8eb28d162e71d1dfe5d34c4
Prateek Soni <prateek_soni@outlook.com>
2022-06-19 16:01:30 +0530

    add meta tags
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,19 @@ const CustomDocument = () => (
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;800&display=swap"
         rel="stylesheet"
       />
+      <meta name="title" content="Prateek Soni | Software Developer" />
+      <meta
+        name="description"
+        content="This is the homepage of my portfolio with details about me, my career and my projects."
+      />
+      <meta
+        name="keywords"
+        content="developer, engineer, portfolio, frontend, backend, fullstack"
+      />
+      <meta name="robots" content="index, follow" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
+      <meta name="author" content="Prateek Soni" />
     </Head>
     <body>
       <Main />
